969e2a3e2efa029d8ccbb7834a434a23311f2d79
serbible <serbible@nate.com>
2019-06-03 11:03:39 +0900

    1. 시작

--- P1_Start/src/main/java/org/zerock/controller/HomeController.java
+++ P1_Start/src/main/java/org/zerock/controller/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("시작");
 		return "home";
 	}
 	
